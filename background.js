22a0615281e3a4502899822fe05cf57f65549229
matthewhadley <matthew.hadley@gmail.com>
2019-10-05 21:16:11 -0700

    fix eslint errors, update README

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getLocation(href) {
-  var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/);
+  var match = href.match(/^(https?:)\/\/(([^:/?#]*)(?::([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/);
   return match && {
     protocol: match[1],
     host: match[2],
@@ -14,12 +14,14 @@ function getLocation(href) {
 }
 
 function popSubdomain(str) {
-  var newName = str.replace(/[^.]*\./, '')
-  return str !== newName && newName
+  var newName = str.replace(/[^.]*\./, '');
+  return str !== newName && newName;
 }
 
 function insertCSS(tabId, filename) {
-  if (!filename) return;
+  if (!filename) {
+    return;
+  }
 
   chrome.tabs.insertCSS(tabId, {
     file: 'chromedotfiles/' + filename + '.css',
@@ -27,20 +29,22 @@ function insertCSS(tabId, filename) {
     allFrames: true
   }, function (res) {
     if (chrome.runtime.lastError) {
-      insertCSS(tabId, popSubdomain(filename))
+      insertCSS(tabId, popSubdomain(filename));
       return;
     }
   });
 }
 
 function executeScript(tabId, filename) {
-  if (!filename) return;
+  if (!filename) {
+    return;
+  }
 
   chrome.tabs.executeScript(tabId, {
     file: 'chromedotfiles/' + filename + '.js'
   }, function(res) {
     if (chrome.runtime.lastError) {
-      executeScript(tabId, popSubdomain(filename))
+      executeScript(tabId, popSubdomain(filename));
       return;
     }
   });
@@ -52,18 +56,18 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   // load css early for no visible delays
   if (changeInfo.status === 'loading') {
     // attempt to insert domain specific css
-    insertCSS(tabId, 'default')
+    insertCSS(tabId, 'default');
     if (match) {
-      insertCSS(tabId, match.hostname)
+      insertCSS(tabId, match.hostname);
     }
   }
 
   // load js
   if (changeInfo.status === 'complete') {
     // attempt to execute default js
-    executeScript(tabId, 'default')
+    executeScript(tabId, 'default');
     if (match) {
-      executeScript(tabId, match.hostname)
+      executeScript(tabId, match.hostname);
     }
   }
 
